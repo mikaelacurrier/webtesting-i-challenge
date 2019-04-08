2b9843e08667ab91e66a22fbf7db61d1a71f3cdb
mikaelacurrier <mikaela.currier@gmail.com>
2019-04-08 14:32:50 -0400

    finished testings

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -5,16 +5,29 @@ module.exports = {
   get,
 };
 
+
 function succeed(item) {
-  return { ...item };
+  return item.enhancement >= 20 ? {...item} : { 
+    ...item,
+    enhancement: item.enhancement + 1
+   };
 }
 
 function fail(item) {
-  return { ...item };
+if(item.enhancement >= 16) {
+  return {...item, enhancement: item.enhancement -1, durability: item.durability -10}
+  } else if(item.enhancement < 15) {
+    return {...item, durability: item.durability -5}
+  } else if(item.enhancement >= 15) {
+    return {...item, durability: item.durability -10}
+}
 }
 
 function repair(item) {
-  return { ...item };
+  return {
+    ...item,
+    durability: 100
+   };
 }
 
 function get(item) {
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,43 @@
 const enhancer = require('./enhancer.js');
 // test away!
+
+it('returns duration to 100', () => {
+    expect(enhancer.repair({ name: 'tester',
+    durability: 90,
+    enhancement: 4}))
+    .toBe({ name: 'tester',
+    durability: 100,
+    enhancement: 4})
+})
+
+testItem = {
+    name: 'tester',
+    durability: 90,
+    enhancement: 4
+}
+
+it('success', () => {
+    expect(enhancer.succeed({ name: 'tester',
+    durability: 90,
+    enhancement: 4}))
+    .toBe({ name: 'tester',
+    durability: 90,
+    enhancement: 5})
+})
+it('success', () => {
+    expect(enhancer.succeed({ name: 'tester',
+    durability: 90,
+    enhancement: 20}))
+    .toBe({ name: 'tester',
+    durability: 90,
+    enhancement: 20})
+})
+
+it('fail', () => {
+    expect(enhancer.fail({name: 'tester',
+    durability: 90,
+    enhancement: 20}))
+    .toBe({ name: 'tester',
+    durability: 80,
+    enhancement: 19})
+})
